aa1a661be6abf1555eb02e2035c98d8b57df4dc8
Synzenn <velriel42@gmail.com>
2024-06-04 11:06:47 +0700

    membuat try

--- Part4/Part4.cpp
+++ Part4/Part4.cpp
@@ -7,6 +7,13 @@ using namespace std;
 
 int main() {
     cout << "Awal Program" << endl; // penanda 1:...
-    
+    try {
+        array<int, 3> data = { 1, 2, 3 };
+        // pesan array integer 3 elemen
+        //vout<<data.at[5]<<endl;
+        //memanggil array elemen ke 5
+        cout << data.at(5) << endl;
+    }
+   
 
 }
